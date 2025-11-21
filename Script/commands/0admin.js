26331177390b7c72557614e1a977bb8d113ad760
dnramim8-ops <dnramim8@gmail.com>
2025-11-21 15:16:10 +0600

    Rename command from '0admin' to 'RAMIM'

--- Script/commands/0admin.js
+++ Script/commands/0admin.js
@@ -1,9 +1,9 @@
 var request = require("request");const { readdirSync, readFileSync, writeFileSync, existsSync, copySync, createWriteStream, createReadStream } = require("fs-extra");
 module.exports.config = {
-	name: "0admin",
+	name: "RAMIM",
 	version: "1.0.5",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "𝐂𝐘𝐁𝐄𝐑_🎀ƒꪖнɪꪑꫝ",
 	description: "Admin Config",
 	commandCategory: "Admin",
 	usages: "Admin",
