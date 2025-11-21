f65dc8f67bd4cb1b7dcaeef2a8867ca158d5a41a
dnramim8-ops <dnramim8@gmail.com>
2025-11-21 06:50:10 +0600

    Ramim

--- Script/commands/0admin.js
+++ Script/commands/0admin.js
@@ -1,9 +1,9 @@
 var request = require("request");const { readdirSync, readFileSync, writeFileSync, existsSync, copySync, createWriteStream, createReadStream } = require("fs-extra");
 module.exports.config = {
-	name: "0admin",
+	name: "Itz Oggy",
 	version: "1.0.5",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "𝐂𝐘𝐁𝐄𝐑 Fahima",
 	description: "Admin Config",
 	commandCategory: "Admin",
 	usages: "Admin",
