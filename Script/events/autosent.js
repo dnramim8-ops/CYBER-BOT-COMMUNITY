5975a03c0bfc981de4191b2d2c18e9639d745956
dnramim8-ops <dnramim8@gmail.com>
2025-11-22 07:56:08 +0600

    Update autosent.js

--- Script/events/autosent.js
+++ Script/events/autosent.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "autosend",
  eventType: [],
  version: "0.0.1",
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ credits: "🦨Kathbirali🐿️",
  description: "Listen events"
 };
 
